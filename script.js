a0eb9cae84ffbfc404e148c70e2b8efd8903e8e5
roccolotuf-spec <roccolotuf@gmail.com>
2026-06-15 13:31:34 +0200

    Add files via upload
    Caricamento iniziale del progetto

---
+++ script.js
@@ -0,0 +1,7 @@
+// Seleziona l'elemento bottone
+const bottone = document.getElementById('mioBottone');
+
+// Aggiunge l'evento al click
+bottone.addEventListener('click', () => {
+    alert('Funziona tutto perfettamente!');
+});
